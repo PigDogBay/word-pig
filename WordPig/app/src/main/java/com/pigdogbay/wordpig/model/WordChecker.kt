2aea1ab116b69ca0c2a1409a5af4d546d2572d44
Mark Bailey <markpdbailey@gmail.com>
2024-05-02 12:35:06 +0100

    Tidied up the WordChecker.kt

--- WordPig/app/src/main/java/com/pigdogbay/wordpig/model/WordChecker.kt
+++ WordPig/app/src/main/java/com/pigdogbay/wordpig/model/WordChecker.kt
@@ -3,9 +3,8 @@ package com.pigdogbay.wordpig.model
 /**
  * Created by Mark on 02/04/2015.
  */
-class WordChecker(var wordList: List<String>) {
+class WordChecker(private var wordList: List<String>) {
     fun isWord(candidate: String): Boolean {
-//        int len = candidate.length();
         for (word in wordList) {
             if (word == candidate) {
                 return true
